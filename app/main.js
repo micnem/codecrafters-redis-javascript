cef21ec96eb129a3d05b563f976b717065e29101
Michael Nemni <micnem@Admins-MacBook-Pro.local>
2023-02-07 20:38:12 +0200

    test connection

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+    connection.on("data", (data) => {
+        console.log('starting!!!!')
+        console.log(data)
+    })
 });
 
 server.listen(6379, "127.0.0.1");
